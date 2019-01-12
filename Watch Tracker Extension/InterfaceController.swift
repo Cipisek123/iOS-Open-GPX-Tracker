5dce11dfdb9b87075c951210e588f6e8bdb10048
Vincent Neo <23420208+vincentneo@users.noreply.github.com>
2019-01-12 14:35:54 +0800

    copy bits of code to watch variant

--- Watch Tracker Extension/InterfaceController.swift
+++ Watch Tracker Extension/InterfaceController.swift
@@ -11,6 +11,12 @@ import Foundation
 
 
 class InterfaceController: WKInterfaceController {
+    
+    @IBOutlet weak var newPinButton: WKInterfaceButton!
+    @IBOutlet weak var resetButton: WKInterfaceButton!
+    @IBOutlet weak var saveButton: WKInterfaceButton!
+    @IBOutlet weak var trackerButton: WKInterfaceButton!
+    @IBOutlet weak var followUserButton: WKInterfaceButton!
 
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
@@ -26,6 +32,16 @@ class InterfaceController: WKInterfaceController {
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
+    }
+    
+    ///
+    /// It adds a Pin (Waypoint/Annotation) to current user location.
+    ///
+    @objc func addPinAtMyLocation() {
+        print("Adding Pin at my location")
+        let waypoint = GPXWaypoint(coordinate: map.userLocation.coordinate)
+        map.addWaypoint(waypoint)
+        self.hasWaypoints = true
     }
 
 }
